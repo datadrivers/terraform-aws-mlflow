c69165454d5b10c711d95159a6650624dbd08438
Guillem Reig <guillem.reig@glovoapp.com>
2023-02-27 17:30:14 +0100

    [DEVEX-508] Improve TF 1.3.x compatibility

---
+++ provider.tf
@@ -0,0 +1,15 @@
+terraform {
+
+  required_version = "~> 1.3"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.2.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 2.3.0"
+    }
+  }
+}
